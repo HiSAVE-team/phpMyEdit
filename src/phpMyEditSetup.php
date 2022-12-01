ddf98149343103b0e6d151a2c436e70e622a2763
Yves DB <yves-dev@internetcontact.be>
2022-12-01 02:06:54 +0200

    Migrate html standard to v5 & utf8, add viewport information.

--- src/phpMyEditSetup.php
+++ src/phpMyEditSetup.php
@@ -22,11 +22,11 @@
 /* $Platon: phpMyEdit/phpMyEditSetup.php,v 1.50 2007-09-16 12:57:07 nepto Exp $ */
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-            "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>phpMyEdit Setup</title>
     <link rel="stylesheet" href="phpMyEditSetup.css?v=5.7.1" />
 </head>
@@ -497,11 +497,11 @@ END;
 
     if ($HTMLissues) {
         $buffer = <<<END
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>$pageTitle</title>
 $css_directive
 </head>
